212a1e092230d676d34b5dcfd88901d067c2a02b
Biruk Birhanu <biruk.ayele.birhanu@gmail.com>
2023-06-20 03:49:45 -0700

    I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print all alphabet in lowercase
+ */
+
+void print_alphabet(void)
+{
+	char My;
+
+	for (My = 'a'; My <= 'z'; My++)
+		_putchar(My);
+
+	_putchar('\n');
+}
